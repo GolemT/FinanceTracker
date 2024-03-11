a3ee38ac24ed8c9ef7e976dbcd07d28976945ae0
GolemT <tkosleck@gmail.com>
2024-03-11 22:02:44 +0100

    nextjs init

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import Link from 'next/link';
+import { randomID } from '../components/logic' 
+import Layout from '../components/Layout';
+ 
+export default function Home() {
+  const num = randomID()
+  return (
+    <Layout>
+      This is the dashboard
+    </Layout>
+  );
+}
