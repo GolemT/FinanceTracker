cff37f4438266dd411b8135cc84b3120f3e1efe1
GolemT <tkosleck@gmail.com>
2024-03-28 23:46:47 +0100

    commented goals/ new test data

--- components/sidemenu.js
+++ components/sidemenu.js
@@ -22,9 +22,9 @@ export default function sidemenu() {
                 <Link href="/list" passHref className={styles.Link}>
                     <span style={getLinkStyle('/list')}>List</span>
                 </Link>
-                <Link href="/goals" passHref className={styles.Link}>
+                {/* <Link href="/goals" passHref className={styles.Link}>
                     <span style={getLinkStyle('/goals')}>Goals</span>
-                </Link>
+                </Link> */}
                 <Link href="/tags" passHref className={styles.Link}>
                     <span style={getLinkStyle('/tags')}>Tags</span>
                 </Link>
