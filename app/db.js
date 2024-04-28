cb51dd0f385e24e81716699fcb26004394b93c0b
GolemT <tkosleck@gmail.com>
2024-04-28 22:25:17 +0200

    edit credential name

--- app/db.js
+++ app/db.js
@@ -3,7 +3,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 
 dotenv.config();
 
-const uri = process.env.MONGODB;
+const uri = process.env.MONGODB_URI;
 
 const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1
